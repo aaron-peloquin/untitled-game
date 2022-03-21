1c426c4bb2c655bdfccefb3dc06f4e300bfbf4bf
aaron-peloquin <peloquin.aaron@gmail.com>
2022-03-20 23:08:29 -0500

    create mercenary library

---
+++ libs/mercenary/src/lib/Mercenary.spec.ts
@@ -0,0 +1,7 @@
+import { mercenary } from './Mercenary';
+
+describe('mercenary', () => {
+  it('should work', () => {
+    expect(mercenary()).toEqual('mercenary');
+  });
+});
---
+++ libs/mercenary/src/lib/Mercenary.ts
@@ -0,0 +1,3 @@
+export function mercenary(): string {
+  return 'mercenary';
+}
