321a2b9ed068ad424586ea64badf950909a66dbc
aaron-peloquin <peloquin.aaron@gmail.com>
2022-03-20 23:06:20 -0500

    wip on quests

--- libs/quest/src/lib/generateQuest.ts
+++ libs/quest/src/lib/generateQuest.ts
@@ -1,49 +1,71 @@
-import _ from 'lodash';
-
 interface I_Quest {
-  title: string
   type: string
+  level: number
+  target: {
+    name: string
+    ethnicity: string
+    profession: string
+  }
 }
 
-const pickRandom = (arr: string[]):string => {
+const pickArray = (arr: string[]):string => {
   return arr[Math.floor(Math.random()*arr.length)];
 };
 
-const QUEST_TYPES = [
-  'Slay',
-  'Capture',
-];
+type pickObjectSig = <T>(obj: Record<string, T>) => [string, T]
 
-const QUEST_DESCRIPTORS = {
-  'Slay': ['Kill', 'Eliminate', 'Destroy'],
-  'Capture': ['Capture', 'Retrieve'],
+const pickObject: pickObjectSig = (obj) => {
+  const keys = Object.keys(obj);
+  const key = pickArray(keys);
+  const value = obj[key];
+  return [key, value];
 };
 
-const QUEST_TARGET_TITLES = [
-  'Sir',
-  'Bandit',
-  'Knave',
-  'Thief',
-  'Rogue',
-  'Troublemaker',
+const QUEST_TYPE = [
+  'Slay',
+  'Capture',
+  'Follow',
 ];
+const QUEST_TARGET_NAMES: Record<string, string[]> = {
+  Human: [
+    'Liam', 'Olivia', 'Noah', 'Emma',
+    'Oliver', 'Ava', 'Elijah', 'Charlotte',
+    'William', 'Sophia', 'James', 'Amelia',
+    'Benjamin', 'Isabella', 'Lucas', 'Mia',
+    'Henry', 'Evelyn', 'Alexander', 'Harper',
+  ],
+  Goblin: [
+    'Krug', 'Nob', 'Ruz', 'Mul',
+    'Geez', 'Gop', 'Stix', 'Ag',
+  ],
+};
 
-const QUEST_TARGET_NAMES = [
-  'Liam', 'Olivia', 'Noah', 'Emma',
-  'Oliver', 'Ava', 'Elijah', 'Charlotte',
-  'William', 'Sophia', 'James', 'Amelia',
-  'Benjamin', 'Isabella', 'Lucas', 'Mia',
-  'Henry', 'Evelyn', 'Alexander', 'Harper',
-];
+const QUEST_TARGET_PROFESSIONS: Record<string, string[]> = {
+  Human: [
+    'Blacksmith', 'Brewer', 'Farmer', 'Fisher',
+    'Noble', 'Bandit', 'Thief', 'Tavernkeeper',
+  ],
+  Goblin: [
+    'Spearmaker', 'Scout', 'Thief', 'Bandit',
+  ],
+};
 
-export const generateQuest = ():I_Quest => {
-  const type = pickRandom(QUEST_TYPES);
-  const typeDescriptor = pickRandom(_.get(QUEST_DESCRIPTORS, type));
-  const targetTitle= pickRandom(QUEST_TARGET_TITLES);
-  const targetName = pickRandom(QUEST_TARGET_NAMES);
-  const title = `${typeDescriptor} ${targetTitle} ${targetName}`;
+export const generateQuest = (questMin=0.6, questMax=1.75):I_Quest => {
+  const level = (Math.random() * (questMax - questMin) + questMin).toFixed(2);
+  const type = pickArray(QUEST_TYPE);
+
+  const [ethnicity, targetNames] = pickObject(QUEST_TARGET_NAMES);
+  const name = pickArray(targetNames);
+  const profession = pickArray(QUEST_TARGET_PROFESSIONS[ethnicity]);
+
+
   return {
-    title,
     type,
+    level: parseFloat(level),
+    target: {
+      ethnicity,
+      name,
+      profession,
+    },
   };
 };
