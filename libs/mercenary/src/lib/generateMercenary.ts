40238f579fd36f1e071ce479255b825c3ddde498
aaron-peloquin <peloquin.aaron@gmail.com>
2022-03-25 23:06:29 -0500

    rework how stats work

--- libs/mercenary/src/lib/generateMercenary.ts
+++ libs/mercenary/src/lib/generateMercenary.ts
@@ -1,6 +1,6 @@
 import {pickArray, pickObject, pickRange} from '@helper';
-import {MERC_CLASS, MERC_CLASS_BASE_STATS, MERC_NAMES} from '@static';
-import {I_Mercenary} from 'TS_Mercenary';
+import {MERC_CLASS, MERC_CLASS_BASE_STATS, MERC_ETHNICITY_BASE_STATS, MERC_NAMES} from '@static';
+import { I_Mercenary, T_BaseStats } from 'TS_Mercenary';
 
 export const generateMercenary = (numberGenerator: () => number = Math.random) => (levelMin=1, levelMax=3):I_Mercenary => {
   const rangeGenerator = pickRange(numberGenerator);
@@ -10,7 +10,12 @@ export const generateMercenary = (numberGenerator: () => number = Math.random) =
   const name = pickArray(MERC_NAMES[ethnicity], numberGenerator);
   const profession = pickArray(mercClasses, numberGenerator);
 
-  const baseStats = MERC_CLASS_BASE_STATS[profession];
+  const baseClassStats = MERC_CLASS_BASE_STATS[profession];
+  const baseStats:T_BaseStats = {...MERC_ETHNICITY_BASE_STATS[ethnicity]}
+  for (const statKey in baseClassStats) {
+    baseStats[statKey] += baseClassStats[statKey]
+  }
+  console.log('baseStats', profession, ethnicity, baseStats)
   const dmgLow = baseStats.damage ^ level;
   const dmgHigh = dmgLow * baseStats.damageMultiplier;
   const daWizard = [
--- libs/static/src/lib/data/MERCENARY.ts
+++ libs/static/src/lib/data/MERCENARY.ts
@@ -7,14 +7,20 @@ export const MERC_CLASS: Record<string, string[]> = {
   Human: ['Fighter', 'Cleric', 'Ranger', 'Thief', 'Wizard'],
 };
 
+export const MERC_ETHNICITY_BASE_STATS: Record<string, T_BaseStats> = {
+  Dwarf: {capture: 0, cost: 2, attack: 15, endurance: 2, stealth: -5},
+  Elf: {capture: 15, cost: 2, attack: -5, endurance: 0, stealth: 15},
+  Human: {capture: 10, cost: 0, attack: 10, endurance: 1, stealth: 10},
+}
+
 export const MERC_CLASS_BASE_STATS: Record<string, T_BaseStats> = {
-  Barbarian: {capture: 30, cost: 6, damage: 11, damageMultiplier: 1, endurance: 6, enduranceMultiplier: 1.5, stealth: 30},
-  Cleric: {capture: 30, cost: 5, damage: 5, damageMultiplier: 1, endurance: 15, enduranceMultiplier: 1.5, stealth: 30},
-  Druid: {capture: 30, cost: 5, damage: 5, damageMultiplier: 1, endurance: 15, enduranceMultiplier: 1.5, stealth: 30},
-  Fighter: {capture: 30, cost: 15, damage: 10, damageMultiplier: 1, endurance: 10, enduranceMultiplier: 1.5, stealth: 30},
-  Ranger: {capture: 30, cost: 12, damage: 15, damageMultiplier: 1, endurance: 8, enduranceMultiplier: 1.5, stealth: 30},
-  Thief: {capture: 30, cost: 5, damage: 20, damageMultiplier: 1, endurance: 5, enduranceMultiplier: 1.5, stealth: 30},
-  Wizard: {capture: 30, cost: 5, damage: 5, damageMultiplier: 1, endurance: 10, enduranceMultiplier: 1, stealth: 30},
+  Barbarian: {capture: 15, cost: 2, attack: 75, endurance: 2, stealth: 5},
+  Cleric: {capture: 80, cost: 2, attack: 20, endurance: 5, stealth: 15},
+  Druid: {capture: 65, cost: 2, attack: 15, endurance: 4, stealth: 30},
+  Fighter: {capture: 70, cost: 2, attack: 35, endurance: 8, stealth: 15},
+  Ranger: {capture: 40, cost: 2, attack: 15, endurance: 5, stealth: 70},
+  Thief: {capture: 25, cost: 2, attack: 20, endurance: 4, stealth: 85},
+  Wizard: {capture: 35, cost: 2, attack: 85, endurance: 1, stealth: 10},
 };
 
 export const MERC_NAMES: Record<string, string[]> = {
