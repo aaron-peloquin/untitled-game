129d2521e9bec519a2b648393c280d063e5fe2e1
aaron-peloquin <peloquin.aaron@gmail.com>
2022-04-07 16:17:35 -0500

    Tabs demo

--- libs/components-game/src/lib/organisms/Location.tsx
+++ libs/components-game/src/lib/organisms/Location.tsx
@@ -1,6 +1,6 @@
 import {Card, Tab, Tabs} from '@components-layout';
 import {useDataLocation} from '@helper';
-import {memo} from 'react';
+import {memo, useState} from 'react';
 
 import MercenaryList from '../molecules/MercenaryList';
 import QuestList from '../molecules/QuestList';
@@ -11,27 +11,28 @@ type Props = {
 }
 const Location: React.FC<Props> = ({locationId}) => {
   const location = useDataLocation(locationId);
-  /**
-   * <TabsProvider tabs={['name', 'name', 'name']}>
-   *  <TabSelector />
-   *  <TabArea name="a">content...</Tab>
-   *  <TabArea name="b">content...</Tab>
-   * </TabsProvider>
-  */
+  const [showTab, setShowTab] = useState(false);
+  const [sortTab, setSortTab] = useState(2);
+
   return <Card heading={location.name} layer="2">
     You're at the local tavern, there are both recruits and citizens with quests available here. This a level {location?.levelRanges?.[0]} to {location?.levelRanges?.[1]} tavern.
+    <br /><button onClick={() => setSortTab(sortTab === 5 ? 2 : 5)}>Sort Quest Tab</button>
+    <br /><button onClick={() => setShowTab(!showTab)}>Toggle Extra Tab</button>
     <Tabs layer="3">
-      <Tab name='Mercenary' id='merc'>
+      <Tab name="Mercenary" id='merc' sort={1}>
         <Card heading='Mercenaries' layer="3">
           {location.mercenaries?.length ? <MercenaryList canHire mercenaries={location.mercenaries} /> : <span>No mercenaries for hire</span>}
         </Card>
       </Tab>
-      <Tab name="Available Quests" id="quests">
+      <Tab name="Available Quests" id="quests" sort={sortTab}>
         <Card heading='Available Quests' layer="3">
           {location.quests?.length ? <QuestList quests={location.quests} /> : <span>No quests available</span>}
         </Card>
       </Tab>
-      <Tab name="Nearby Locations" id="locz">
+      {showTab && <Tab name="Another Tab" id="another" sort={3}>
+        Some other tab contents
+      </Tab>}
+      <Tab name="Nearby Locations" id="locz" sort={4}>
         <Card heading='Travel' layer="3">
           {location.relatedLocations?.length ? <RelatedLocationsList locations={location.relatedLocations} /> : <span>End of the road?</span>}
         </Card>
