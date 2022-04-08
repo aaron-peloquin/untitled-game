5ae5d10aa9b1108608f767b91b64a4aa50976786
aaron-peloquin <peloquin.aaron@gmail.com>
2022-04-07 22:17:12 -0500

    capitalization

--- libs/components-game/src/lib/organisms/TopNav.tsx
+++ libs/components-game/src/lib/organisms/TopNav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import {memo} from 'react';
 
 export const TopNav = memo(() => {
-  return <Card layer='1' heading="An untitled Game">
+  return <Card layer='1' heading="An Untitled Game">
     <GridTemplate columns={2}>
       <GridArea><Link href={URLs.mainMenu}>Main Menu</Link></GridArea>
       <GridArea><Link href={URLs.settings}>Settings</Link></GridArea>
