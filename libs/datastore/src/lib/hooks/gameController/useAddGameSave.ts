ea81443d8baa5b76586dd91b8a5a4f808903f515
aaron-peloquin <peloquin.aaron@gmail.com>
2022-04-09 17:42:59 -0500

    add omit and type checking

--- libs/datastore/src/lib/hooks/gameController/useAddGameSave.ts
+++ libs/datastore/src/lib/hooks/gameController/useAddGameSave.ts
@@ -8,7 +8,7 @@ import {gameController} from '../../datastores/gameController';
 export const useAddGameSave = (landingPage?: string) => {
   const setCurrentGameSave = useSetCurrentGameSave(landingPage);
   return useCallback((name: string, seed: string) => {
-    const newSave: T_GameSave = {
+    const newSave: Omit<T_GameSave, 'gameSaveId'> = {
       currentLocation: 0,
       currentSave: 0,
       gameDatastoreName: '',
@@ -16,6 +16,9 @@ export const useAddGameSave = (landingPage?: string) => {
       pastLocations: [],
       seed,
     };
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     gameController.gameSaves.add(newSave).then((id) => {
       const gameDatastoreName = `${id}_${name}`.replace(/[^a-zA-Z0-9_]/g, '-').toLowerCase();
       gameController.gameSaves.update(id, {gameDatastoreName}).then(() => {
