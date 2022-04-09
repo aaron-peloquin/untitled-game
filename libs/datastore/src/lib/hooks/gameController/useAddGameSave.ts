fecc893b0ae9f368a0e406ddf0fdea47d5570062
aaron-peloquin <peloquin.aaron@gmail.com>
2022-04-09 01:20:31 -0500

    create hook for adding games

---
+++ libs/datastore/src/lib/hooks/gameController/useAddGameSave.ts
@@ -0,0 +1,26 @@
+import {useCallback} from 'react';
+import {T_GameSave} from 'TS_General';
+
+import {useSetCurrentGameSave} from './useSetCurrentGameSave';
+
+import {gameController} from '../../datastores/gameController';
+
+export const useAddGameSave = (landingPage?: string) => {
+  const setCurrentGameSave = useSetCurrentGameSave(landingPage);
+  return useCallback((name: string, seed: string) => {
+    const newSave: T_GameSave = {
+      currentLocation: 0,
+      currentSave: 0,
+      gameDatastoreName: '',
+      name,
+      pastLocations: [],
+      seed,
+    };
+    gameController.gameSaves.add(newSave).then((id) => {
+      const gameDatastoreName = `${id}_${name}_${seed}`.replace(/[^a-zA-Z0-9_]/g, '').toLowerCase();
+      gameController.gameSaves.update(id, {gameDatastoreName}).then(() => {
+        setCurrentGameSave(id);
+      });
+    });
+  }, [setCurrentGameSave]);
+};
