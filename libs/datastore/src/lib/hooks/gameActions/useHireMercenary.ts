c9c791118ef17c0cfc736e0cda417a4c6b4750c6
aaron-peloquin <peloquin.aaron@gmail.com>
2022-04-23 21:37:08 -0500

    console log

--- libs/datastore/src/lib/hooks/gameActions/useHireMercenary.ts
+++ libs/datastore/src/lib/hooks/gameActions/useHireMercenary.ts
@@ -21,7 +21,6 @@ export const useHireMercenary = (mercenary: T_Mercenary, hireCost: number) => {
     const newGoldValue = bandGold - hireCost;
 
     if (gameData?.dataStore && bandId > 0 && newGoldValue >= 0) {
-      console.log({mercenaryId});
       // reduce gold
       gameData.dataStore.band.update(bandId, {gold: newGoldValue});
       // show mercenary stats
