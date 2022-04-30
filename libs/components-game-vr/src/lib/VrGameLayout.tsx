521895c6c8bc01d243ee1fd2ead4452048533454
aaron-peloquin <peloquin.aaron@gmail.com>
2022-04-30 09:53:00 -0500

    add components-game-vr library

---
+++ libs/components-game-vr/src/lib/VrGameLayout.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ComponentsGameVrProps {}
+
+const VrGameLayout = (props: ComponentsGameVrProps) => {
+  return (
+    <div>
+      <h1>Welcome to ComponentsGameVr!</h1>
+    </div>
+  );
+};
+
+export default VrGameLayout;
