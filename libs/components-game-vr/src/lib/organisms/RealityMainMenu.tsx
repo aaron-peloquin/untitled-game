bd272b00cfce887ad48cd0ee27c917b4d2fcf922
aaron-peloquin <peloquin.aaron@gmail.com>
2022-05-01 00:14:18 -0500

    add key prop

--- libs/components-game-vr/src/lib/organisms/RealityMainMenu.tsx
+++ libs/components-game-vr/src/lib/organisms/RealityMainMenu.tsx
@@ -6,7 +6,7 @@ const RealityMainMenu: React.FC = () => {
   const gameSaves = useListGameSaves();
 
   return <group>{gameSaves?.map((gameSave, index) => {
-    return <group position={[0, .5 * index, .5]}>
+    return <group position={[0, .5 * index, .5]} key={gameSave.gameSaveId}>
       <RealitySaveGameItem gameSave={gameSave} index={index} />
     </group>;
   })}</group>;
