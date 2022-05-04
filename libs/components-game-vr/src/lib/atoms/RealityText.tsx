6270f679f7a48c80cb79967fe70f480445be1000
aaron-peloquin <peloquin.aaron@gmail.com>
2022-05-04 06:57:29 -0500

    ts cleanup

--- libs/components-game-vr/src/lib/atoms/RealityText.tsx
+++ libs/components-game-vr/src/lib/atoms/RealityText.tsx
@@ -8,6 +8,7 @@ export type T_Props = {
     color?: ReactThreeFiber.Color
     textAlign?: 'left' | 'right' | 'center' | 'justify'
     font?: string
+    position?: ReactThreeFiber.Vector3
   }
 const RealityText = forwardRef<any, T_Props>(({text, fontSize = .5, ...props}, ref) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
