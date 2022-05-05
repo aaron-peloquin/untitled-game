db222078c0a2a084f8e32b9d3b6565a4e2619e7b
aaron-peloquin <peloquin.aaron@gmail.com>
2022-05-04 22:51:48 -0500

    implement on GrabBall test component

--- libs/components-game-vr/src/lib/atoms/GrabBall.tsx
+++ libs/components-game-vr/src/lib/atoms/GrabBall.tsx
@@ -1,27 +1,11 @@
+import {useGrab} from '@helper';
 import {Sphere} from '@react-three/drei';
-import {useInteraction} from '@react-three/xr';
-import {useRef} from 'react';
 
 const GrabBall = () => {
-  const refBall = useRef();
-  // const left = useController('left');
-  useInteraction(refBall, 'onSqueeze', ({controller, intersection}) => {
-    const controllerPos = controller.controller.position;
-    console.log({controllerPos});
-    intersection?.object.position.set(controllerPos.x, controllerPos.y, controllerPos.z);
-  });
-  // console.log({left});
-  // Load texture (the black plus sign)
-  // Make the ball a physics object with a low mass
-  return (<>
-    {/* <Plane args={[2, 2]} /> */}
-    {/* <RayGrab> */}
-    <Sphere args={[.1, 64, 64]} ref={refBall}>
-      <meshBasicMaterial color="hotpink" />
-    </Sphere>
-    {/* </RayGrab> */}
-    {/* <OrbitControls /> */}
-  </>);
+  const {ref, isGrabbed} = useGrab();
+  return <Sphere args={[.1, 64, 64]} ref={ref}>
+    <meshBasicMaterial color={isGrabbed ? 'red' : 'hotpink'} />
+  </Sphere>;
 };
 
 GrabBall.displayName = 'GrabBall';
