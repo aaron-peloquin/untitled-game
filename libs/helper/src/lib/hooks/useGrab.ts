70e997194af1ab056836c85195f495c7f379bcfe
aaron-peloquin <peloquin.aaron@gmail.com>
2022-05-22 12:12:07 -0500

    send bags to useGrab callbacks

--- libs/helper/src/lib/hooks/useGrab.ts
+++ libs/helper/src/lib/hooks/useGrab.ts
@@ -1,14 +1,15 @@
-import {useInteraction} from '@react-three/xr';
+import {useInteraction, XRInteractionEvent} from '@react-three/xr';
 import {MutableRefObject, useRef, useState} from 'react';
 
-type useGrabSig = (maxDistance?: number, onRelease?: () => void, onGrab?: () => void) => {isGrabbed: boolean, ref: MutableRefObject<any>}
+type useGrabSig = (maxDistance?: number, onRelease?: (onReleaseArgs: XRInteractionEvent) => void, onGrab?: (onGrabArgs:XRInteractionEvent) => void) => {isGrabbed: boolean, ref: MutableRefObject<any>}
 
 export const useGrab: useGrabSig = (maxDistance = 0.075, onRelease, onGrab) => {
   const ref = useRef<any>();
   const refContainer = useRef<any>();
   const [isGrabbed, setIsGrabbed] = useState(false);
 
-  useInteraction(ref, 'onSqueezeStart', ({controller, intersection}) => {
+  useInteraction(ref, 'onSqueezeStart', (onGrabArgs) => {
+    const {controller, intersection} = onGrabArgs;
     const controllerApi = controller.controller;
     if (intersection && intersection?.distance <= maxDistance) {
       const objParent = intersection.object.parent;
@@ -17,18 +18,19 @@ export const useGrab: useGrabSig = (maxDistance = 0.075, onRelease, onGrab) => {
       }
       setIsGrabbed(true);
       if (onGrab) {
-        onGrab();
+        onGrab(onGrabArgs);
       }
       controllerApi.attach(intersection.object);
     }
   });
 
-  useInteraction(ref, 'onSqueezeEnd', ({intersection}) => {
+  useInteraction(ref, 'onSqueezeEnd', (onReleaseArgs) => {
+    const {intersection} = onReleaseArgs;
     if (refContainer.current && intersection) {
       setIsGrabbed(false);
       refContainer.current.attach(intersection.object);
       if (onRelease) {
-        onRelease();
+        onRelease(onReleaseArgs);
       }
     }
   });
