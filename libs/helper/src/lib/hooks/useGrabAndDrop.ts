d5e319daa9934247c9a5332a1d7364cf0ebaee32
aaron-peloquin <peloquin.aaron@gmail.com>
2022-06-12 01:35:50 -0500

    multiple drop target support

--- libs/helper/src/lib/hooks/useGrabAndDrop.ts
+++ libs/helper/src/lib/hooks/useGrabAndDrop.ts
@@ -4,20 +4,29 @@ import {Mesh, Vector3} from 'three';
 
 import {useGrab} from './useGrab';
 
-export const useGrabAndDrop = (refReceiverBox: MutableRefObject<Mesh | undefined>, handleDrop:(distance: number) => void, grabbableDistance?: number) => {
+type T_DropTargets = {
+  refReceiverBox: MutableRefObject<Mesh | undefined>
+  handleDrop:(distance: number) => void
+}
+
+export const useGrabAndDrop = (dropTargets: T_DropTargets[], grabbableDistance?: number) => {
   const onRelease = useCallback((releaseArgs: XRInteractionEvent) => {
     const object = releaseArgs.intersection?.object;
 
-    if (refReceiverBox.current && object) {
-      refReceiverBox.current.geometry.computeBoundingBox();
-      const positions = {obj: new Vector3(0, 0, 0), rec: new Vector3(0, 0, 0)};
-      object.getWorldPosition(positions.obj);
-      refReceiverBox.current.getWorldPosition(positions.rec);
-      const distanceToPoint = positions.rec.distanceTo(positions.obj);
+    if (dropTargets.length) {
+      dropTargets.map(({refReceiverBox, handleDrop}) => {
+        if (refReceiverBox.current && object) {
+          refReceiverBox.current.geometry.computeBoundingBox();
+          const positions = {obj: new Vector3(0, 0, 0), rec: new Vector3(0, 0, 0)};
+          object.getWorldPosition(positions.obj);
+          refReceiverBox.current.getWorldPosition(positions.rec);
+          const distanceToPoint = positions.rec.distanceTo(positions.obj);
 
-      handleDrop(distanceToPoint);
+          handleDrop(distanceToPoint);
+        }
+      });
     }
-  }, [handleDrop, refReceiverBox]);
+  }, [dropTargets]);
 
   const {ref: refGrabbableBox, isGrabbed} = useGrab(grabbableDistance, onRelease);
   return {isGrabbed, refGrabbableBox};
