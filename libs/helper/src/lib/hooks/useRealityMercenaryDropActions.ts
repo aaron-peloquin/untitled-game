76a9f631fba32ef659bccc04a0b55b843f2a7dd1
aaron-peloquin <peloquin.aaron@gmail.com>
2022-06-20 13:34:03 -0500

    remove inspection callbacks

--- libs/helper/src/lib/hooks/useRealityMercenaryDropActions.ts
+++ libs/helper/src/lib/hooks/useRealityMercenaryDropActions.ts
@@ -5,20 +5,9 @@ import {useGrabAndDrop} from './useGrabAndDrop';
 
 export const useRealityMercenaryDropActions = (
     refSelectMercenaryBox:MutableRefObject<Mesh | undefined>,
-    refInspectMercenaryBox:MutableRefObject<Mesh | undefined>,
     setSelected: (unset?: boolean | undefined) => void,
-    setInspect: (unset?: boolean | undefined) => void,
     isSelected: boolean,
-    isInspecting: boolean,
 ) => {
-  const handleInspect = useCallback((distance) => {
-    if (distance <= .15) {
-      setInspect();
-    } else if (isInspecting) {
-      setInspect(true);
-    }
-  }, [isInspecting, setInspect]);
-
   const handleSelect = useCallback((distance) => {
     if (distance <= .15) {
       setSelected();
@@ -28,11 +17,10 @@ export const useRealityMercenaryDropActions = (
   }, [isSelected, setSelected]);
 
   const dropTargets = useMemo(() => ([
-    {handleDrop: handleInspect, refReceiverBox: refInspectMercenaryBox},
     {handleDrop: handleSelect, refReceiverBox: refSelectMercenaryBox},
-  ]), [handleInspect, handleSelect, refInspectMercenaryBox, refSelectMercenaryBox]);
+  ]), [handleSelect, refSelectMercenaryBox]);
 
   const {isGrabbed, refGrabbableBox} = useGrabAndDrop(dropTargets);
 
-  return {isGrabbed, isInspecting, isSelected, refGrabbableBox};
+  return {isGrabbed, isSelected, refGrabbableBox};
 };
