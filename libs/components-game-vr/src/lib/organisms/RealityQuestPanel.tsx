9cc2b0cf053554a9131455d625c574586108deff
aaron-peloquin <peloquin.aaron@gmail.com>
2022-07-12 21:41:57 -0500

    add transparent to quest panel

--- libs/components-game-vr/src/lib/organisms/RealityQuestPanel.tsx
+++ libs/components-game-vr/src/lib/organisms/RealityQuestPanel.tsx
@@ -13,7 +13,7 @@ type Props = {
   refSelectBox: MutableRefObject<Mesh | undefined>
 }
 const RealityQuestPanel: React.FC<Props> = ({refSelectBox}) => {
-  const selectBoxPosition = useMemo(() => new Vector3(0, -0.3, .05), []);
+  const selectBoxPosition = useMemo(() => new Vector3(0, -0.4, .05), []);
   const goQuestPosition = useMemo(() => new Vector3(0, -0.34, .01), []);
   const boardPosition = useMemo(() => new Vector3(0, .1, -.001), []);
   const [questResults, setQuestResults] = useState<T_QuestResults>();
@@ -21,7 +21,7 @@ const RealityQuestPanel: React.FC<Props> = ({refSelectBox}) => {
 
   return <>
     <group position={[0, 0, -1]}>
-      <RealityBox color="#533" args={[1.5, 1, 0.001]} position={boardPosition} />
+      <RealityBox color="#533" transparent opacity={0.5} args={[1.5, 1, 0.001]} position={boardPosition} />
       {questResults && <>
         <RealityText text={questResults.outcome} fontSize={0.1} position={[0, .5, 0]} />
         <RealityText text={`Band: +${displayNumber(questResults.band.exp * 100)}% exp, +${questResults.band.gold} gold`} fontSize={0.05} position={[0, .4, 0]} />
