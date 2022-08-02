415fd9cb031594c418f9943649c1b613586215b9
aaron-peloquin <peloquin.aaron@gmail.com>
2022-08-01 22:37:23 -0500

    add grab sound

--- libs/helper/src/lib/hooks/useGrab.ts
+++ libs/helper/src/lib/hooks/useGrab.ts
@@ -3,15 +3,18 @@ import {MutableRefObject, useRef, useState} from 'react';
 
 type useGrabSig = (maxDistance?: number, onRelease?: (onReleaseArgs: XRInteractionEvent) => void, onGrab?: (onGrabArgs:XRInteractionEvent) => void) => {isGrabbed: boolean, ref: MutableRefObject<any>}
 
+
 export const useGrab: useGrabSig = (maxDistance = 0.075, onRelease, onGrab) => {
   const ref = useRef<any>();
   const refContainer = useRef<any>();
   const [isGrabbed, setIsGrabbed] = useState(false);
+  const clickAudio = new Audio('/sfx/mixkit-interface-click-1126.mp3')
 
   useInteraction(ref, 'onSqueezeStart', (onGrabArgs) => {
     const {controller, intersection} = onGrabArgs;
     const controllerApi = controller.controller;
     if (intersection && intersection?.distance <= maxDistance) {
+      clickAudio.play();
       let obj = intersection.object;
       while (obj !== ref.current && obj.parent) {
         obj = obj.parent;
@@ -40,6 +43,7 @@ export const useGrab: useGrabSig = (maxDistance = 0.075, onRelease, onGrab) => {
       }
       if (obj) {
         refContainer.current.attach(obj);
+        clickAudio.play()
         if (onRelease) {
           onRelease(onReleaseArgs);
         }
